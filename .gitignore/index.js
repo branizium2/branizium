5cdbb790d94e7c19a9d6c9bea5fc4d8d723e4bb2
branizium <49403172+branizium@users.noreply.github.com>
2019-06-09 08:19:14 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -66,7 +66,8 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === '!staff') {
-    message.channel.sendMessage('FONDATEUR: Buggup FR;Erachk     /     CO-FONDATEUR: AUCUN     /     CHEF-ADMIN: AUCUN     /     SUPER-ADMIN: AUCUN     /     ADMINISTRATEUR: aucun    /     SUPER-MODO: AUCUN     /     MODERATEUR: agent vegeto     /     HELPEUR: AUCUN     /     STAFF-TEST: AUCUN     /');
+    message.channel.sendMessage('FONDATEUR: Buggup FR;Erachk    
+                                - /     CO-FONDATEUR: AUCUN     /     CHEF-ADMIN: AUCUN     /     SUPER-ADMIN: AUCUN     /     ADMINISTRATEUR: aucun    /     SUPER-MODO: AUCUN     /     MODERATEUR: agent vegeto     /     HELPEUR: AUCUN     /     STAFF-TEST: AUCUN     /');
   }
 });
 
